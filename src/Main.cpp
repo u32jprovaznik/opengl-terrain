984691babfe4de6ef4d089e79129df6746c69360
u32jprovaznik <jprovaznik@proton.me>
2023-05-16 20:27:12 +0200

    Creating the VAO, VBO, and EBO

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,6 +7,24 @@ const unsigned int WINDOW_WIDTH  = 800;
 const unsigned int WINDOW_HEIGHT = 600;
 const char*        WINDOW_TITLE  = "GLFW";
 
+// Callbacks
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+
+// Vertices and Indices
+GLfloat vertices[] = {
+  -0.5f,  -0.5f, 0.f,
+   0.0f,  -0.5f, 0.f,
+   0.5f,  -0.5f, 0.f,
+  -0.25f,  0.0f, 0.f,
+   0.25f,  0.0f, 0.f,
+   0.0f,   0.5f, 0.f,
+};
+GLuint indices[] = {
+  0, 1, 3,
+  1, 2, 4,
+  3, 4, 5,
+};
+
 int main()
 {
   // Initializing GLFW
@@ -50,15 +68,50 @@ int main()
   GLclampf alpha = 1.0f;
   glClearColor(red, blue, green, alpha);
 
+  // Callbacks
+  glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+  // VAO, VBO and EBO
+  GLuint VAO;
+  GLuint VBO;
+  GLuint EBO;
+
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glBindVertexArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
   // Main Loop
   while (!glfwWindowShouldClose(window))
   {
     glfwPollEvents();
     glClear(GL_COLOR_BUFFER_BIT);
+    glBindVertexArray(VAO);
     glfwSwapBuffers(window);
   }
 
   // Terminating the Program
   glfwTerminate();
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
   return 0;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+  glViewport(0, 0, width, height);
 }
